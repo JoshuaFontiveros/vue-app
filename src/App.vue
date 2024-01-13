2697a1892772ef0f54c1871fce41d7383ebe872b
Joshua <fontiveros.joshua@gmail.com>
2024-01-13 22:25:54 +0800

    Feat: vue router and initial navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
+import { RouterView } from 'vue-router';
+
+import Header from './components/Header.vue';
 </script>
 
 <template>
-  <div class="">
-    <a class="" href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Header />
+  <RouterView />
 </template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
 
-defineProps<{ msg: string }>()
+interface HelloWorldProps {
+  msg: string;
+}
 
-const count = ref(0)
+defineProps<HelloWorldProps>();
+
+const count = ref(0);
 </script>
 
 <template>
@@ -19,20 +23,13 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the
+    official Vue + Vite starter
   </p>
   <p>
     Install
     <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="text-5xl font-semibold">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
